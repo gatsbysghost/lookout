67a536d5d8e85b74d847d80d4933bb8ac449b63a
sreu <sreu@cisco.com>
2017-04-26 16:42:31 -0400

    Added url field to query statuses by friendly names instead of object IDs

--- settings.py
+++ settings.py
@@ -24,12 +24,20 @@ overallschema = {
 
 canaries = {
     'item_title': 'canary',
+    'additional_lookup': {
+        'url': 'regex("[\w]+")',
+        'field': 'hostname'
+    },
     'resource_methods': ['GET'],
     'schema': fmcschema
 }
 
 coalmine = {
     'resource_methods': ['GET'],
+    'additional_lookup': {
+        'url': 'regex("[\w]+")',
+        'field': 'name'
+    },
     'schema': overallschema
 }
 
