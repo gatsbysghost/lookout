4e95f1c671456a42c1513915e5d3ee7a6a06aa92
sreu <sreu@cisco.com>
2017-04-26 22:24:22 -0400

    Trying again for REST auth

--- lookoutAPI.py
+++ lookoutAPI.py
@@ -1,12 +1,12 @@
 from eve import Eve
 from eve.auth import BasicAuth
 
-#class PwAuth(BasicAuth):
-#    def check_auth(self, username, password, allowed_roles, resource,
-#                   method):
-#        return username == 'admin' and password == 'cisco123'
+class PwAuth(BasicAuth):
+    def check_auth(self, username, password, allowed_roles, resource,
+                   method):
+        return username == 'cisco' and password == 'cisco'
 
-app = Eve()
+app = Eve(auth=PwAuth)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0',port=5000)
