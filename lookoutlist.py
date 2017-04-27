da89356ff111eac2b4d6b47faae2c55c973bb5ba
sreu <sreu@cisco.com>
2017-04-27 16:52:18 -0400

    Added San Jose-based FMC

--- lookoutlist.py
+++ lookoutlist.py
@@ -6,5 +6,6 @@ from lookout import Fmc
 #hostname=None, ipaddr=None, username='', passwd='', status='ok'
 
 rtpFMC = Fmc('Lookout_RTP_FMC', '172.18.124.211', 'admin', 'S0urceF!reRTP')
+sjFMC = Fmc('Lookout_SJ_FMC', '172.16.53.4', 'admin', 'S0urceF!reSJ')
 
-fmclist = [rtpFMC]
+fmclist = [rtpFMC,sjFMC]
