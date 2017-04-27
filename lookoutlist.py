240867275a59bf519a03f81298441a13ff82a075
sreu <sreu@cisco.com>
2017-04-27 12:47:50 -0400

    correcting creds for RTP FMC

--- lookoutlist.py
+++ lookoutlist.py
@@ -5,6 +5,6 @@ from lookout import Fmc
 #Reminder: Fmc class default order + values:
 #hostname=None, ipaddr=None, username='', passwd='', status='ok'
 
-rtpFMC = Fmc('Lookout_RTP_FMC', '172.18.124.211', 'admin', 'S0urceF1reRTP')
+rtpFMC = Fmc('Lookout_RTP_FMC', '172.18.124.211', 'admin', 'S0urceF!reRTP')
 
 fmclist = [rtpFMC]
