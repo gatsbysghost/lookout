42be080bbcca437ca0ef90608e412a725100109b
sreu <sreu@cisco.com>
2017-04-26 22:20:50 -0400

    commented out auth class and modified username and password

--- lookoutAPI.py
+++ lookoutAPI.py
@@ -1,10 +1,10 @@
 from eve import Eve
 from eve.auth import BasicAuth
 
-class PwAuth(BasicAuth):
-    def check_auth(self, username, password, allowed_roles, resource,
-                   method):
-        return username == 'cisco' and password == '2BlackEyes!'
+#class PwAuth(BasicAuth):
+#    def check_auth(self, username, password, allowed_roles, resource,
+#                   method):
+#        return username == 'admin' and password == 'cisco123'
 
 app = Eve()
 
