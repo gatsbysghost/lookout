5c6d025a6cbe0ec033a304aad48372260fb4af06
sreu <sreu@cisco.com>
2017-05-02 09:59:50 -0400

    Added BGL node to list of monitored FMCs

--- lookoutlist.py
+++ lookoutlist.py
@@ -8,6 +8,6 @@ from lookout import Fmc
 rtpFMC = Fmc('Lookout_RTP_FMC', '172.18.124.211', 'admin', 'S0urceF!reRTP')
 sjFMC = Fmc('Lookout_SJ_FMC', '172.16.53.4', 'admin', 'S0urceF!reSJ')
 #krkFMC = Fmc('Lookout_KRK_FMC', '172.16.53.4', 'admin', 'S0urceF!reKRK')
-#bglFMC = Fmc('Lookout_BGL_FMC', '172.16.53.4', 'admin', 'S0urceF!reBGL')
+bglFMC = Fmc('Lookout_BGL_FMC', '10.76.77.20', 'admin', 'S0urceF!reBGL')
 
-fmclist = [rtpFMC,sjFMC]
+fmclist = [rtpFMC,sjFMC,bglFMC]
