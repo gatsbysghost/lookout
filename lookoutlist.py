322b4cab8ad9552dafb3e344fd4c5366634ed9d5
sreu <sreu@cisco.com>
2017-05-04 11:02:21 -0400

    Added Brussels FMC

--- lookoutlist.py
+++ lookoutlist.py
@@ -9,5 +9,6 @@ rtpFMC = Fmc('Lookout_RTP_FMC', '172.18.124.211', 'admin', 'S0urceF!reRTP')
 sjFMC = Fmc('Lookout_SJ_FMC', '172.16.53.4', 'admin', 'S0urceF!reSJ')
 #krkFMC = Fmc('Lookout_KRK_FMC', '172.16.53.4', 'admin', 'S0urceF!reKRK')
 bglFMC = Fmc('Lookout_BGL_FMC', '10.76.77.20', 'admin', 'S0urceF!reBGL')
+bruFMC = Fmc('Lookout_BRU_FMC', '10.48.18.41', 'admin', 'S0urceF!reBRU')
 
-fmclist = [rtpFMC,sjFMC,bglFMC]
+fmclist = [rtpFMC,sjFMC,bglFMC,bruFMC]
